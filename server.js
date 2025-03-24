a04ce43351adef36137da5b08985e937455901a0
Mario Bravo Pineda <135020003+MarioBravoP@users.noreply.github.com>
2025-03-24 18:21:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,30 @@ const config = require('./config');
 const express = require("express");
 
 const app = express();
-const PORT = config.port || 3000;
-const cors = require('cors')
+const cors = require('cors');
 
-const books_routes = require('./routes/books')
-const publishers_routes = require('./routes/publishers')
-const stores_routes = require('./routes/stores')
-const authors_routes = require('./routes/authors')
-const inventory_routes = require('./routes/inventory')
-const user_routes = require('./routes/users')
+const books_routes = require('./routes/books');
+const publishers_routes = require('./routes/publishers');
+const stores_routes = require('./routes/stores');
+const authors_routes = require('./routes/authors');
+const inventory_routes = require('./routes/inventory');
+const user_routes = require('./routes/users');
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use('/books', books_routes);
 app.use('/publishers', publishers_routes);
 app.use('/stores', stores_routes);
 app.use('/authors', authors_routes);
 app.use('/inventory', inventory_routes);
-app.use('/users', user_routes)
+app.use('/users', user_routes);
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = config.port || 3000;
+    app.listen(PORT, () => {
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
